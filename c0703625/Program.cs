195df3fdbc34edf94b621d55b9356e72d0d54152
visualstudio <visualstudio@230-PC08>
2019-02-07 15:34:46 -0500

    3.30 commit

--- c0703625/Program.cs
+++ c0703625/Program.cs
@@ -39,4 +39,46 @@ namespace c0703625
             return true;
         }
     }
+    class dog
+    {
+        public dog(string name, string breed, dog next_dog, dog previous_dog)
+        {
+            dog_name = name;
+            dog_breed = breed;
+            next_dog = next;
+            previous_dog = previous;
+        }
+        public string dog_name;
+        public string dog_breed;
+            public dog next_dog;
+            public dog previous_dog;
+            
+    }
+    class birthday_party
+    {
+        public dog peanut;
+        public dog fifi;
+        public dog clarence;
+        public dog roy;
+     
+        public dog head;
+        public dog tail;
+        public dog temporary;
+        public void invite()
+        {
+            peanut = new dog("peanut", "Bichon");
+            fifi = new dog("fifi","Poodle");
+            clarence = new dog("clarence","German Sheppard");
+            roy = new dog("roy", "Beagle");\
+
+            peanut.previous_dog = null;
+            peanut.next_dog = fifi;
+            fifi.previous_dog = peanut;
+            fifi.next_dog = clarence;
+            clarence.previous_dog = fifi;
+            clarence.next_dog = roy;
+            roy.previous_dog = clarence;
+            roy.next_dog = null;
+        }
+    }
 }
